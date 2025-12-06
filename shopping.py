5e40a5ca06f7b1d0063a1e491b130dd603f4a305
steam101001101 <nafiunsu462@gmail.com>
2025-12-06 16:59:57 +0000

    Add files via upload

---
+++ shopping.py
@@ -0,0 +1,16 @@
+products={"laptop":315,"table":25,"tv":1502,"chess_board":14,"tennis_racket":41,"shampoo":30,"phone":261,"ps5":1050,"xbox console":502}
+for key,value in products.items():
+    print(key,"£"+str(value))
+cart={}
+while True:
+    buyer=input("what product are you buying? type no if not")
+    if buyer=="no":
+        break
+    if buyer not in products:
+        print("this product is not availble")
+        continue
+    quantity=int(input("how many are you buying?"))
+    cart[buyer]=quantity
+    print(cart)
+for key,value in cart.items():
+    print(key,value,products[key])
