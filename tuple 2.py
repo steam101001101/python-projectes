b0bc74da3eca8abb5d0ddb7d74c639e28bc0c257
steam101001101 <nafiunsu462@gmail.com>
2026-01-03 16:57:16 +0000

    Add files via upload

---
+++ tuple 2.py
@@ -0,0 +1,17 @@
+n_tuple=("mouse",[8,4,6],(1,2,3))
+print(n_tuple)
+
+#retrive an item from a tuple
+print(n_tuple[1][1])
+print(n_tuple[0][3])
+#To print'use from mouse from n_tuple
+print(n_tuple[0][2:5])
+
+my_tuple=('p','r','o','g','r','a','m','i','z')
+print(my_tuple[1:4])
+
+Adress=("24","Buclingham Mansions","353 west end lane","London","NWG 1LR")
+Adress=("23","Buckingham Mansions","353 west end lane","Warsaw","NWG 1LR")
+print('Adress:',Adress)
+print(Adress[3][0:6])
+print('Adress[3:5]:',Adress[3:5])
